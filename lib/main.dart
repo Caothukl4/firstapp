fa4995918ba0c9c48eadfe9d51ac0f1a8de206ea
Phạm Tuấn Anh <tuananhpvpl@gmail.com>
2025-03-05 23:54:46 +0700

    Buttonandicon
    RaiseButton, IconButton

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ void main() {
 
 class Home extends StatelessWidget {
   const Home({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,22 +19,18 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text(
-          "Hello TuanAnh",
-          style: TextStyle(
-            color: Colors.grey,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            fontFamily: 'IndieFlower',
-          ),
-        ),
+        child: IconButton(
+          onPressed: () {
+            print("You Click me ???");
+          },
+          icon: Icon(Icons.email),color: Colors.amber,
+        )
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         backgroundColor: Colors.red[600],
         child: Text(
-          "Click me",
+          "Click",
           style: TextStyle(color: Colors.white),
         ),
       ),
