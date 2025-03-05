6204377d7c8cd6d8b358269f054a0200d7d8c2eb
Phạm Tuấn Anh <tuananhpvpl@gmail.com>
2025-03-05 12:29:39 +0700

    Color,Font(Indie)
    add color and font

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,31 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: Scaffold(
       appBar: AppBar(
         title: Text("My first app"),
         centerTitle: true,
+        backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text("Hello TuanAnh"),
+        child: Text("Hello TuanAnh",
+        style: TextStyle(
+            color: Colors.grey,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            fontFamily: 'IndieFlower'),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {  },
-        child: Text("Click"),
+        backgroundColor: Colors.red[600],
+        child: Text("Click",style: TextStyle(color: Colors.white),
       ),
     )
-  ));
+  ))
+  );
 }
 
 
