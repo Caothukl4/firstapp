fb48f8ffefc6f17631b742ff1fbf8a884f55f855
Phạm Tuấn Anh <tuananhpvpl@gmail.com>
2025-03-05 11:34:02 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("My first app"),
+        centerTitle: true,
+      ),
+      body: Text("Hello"),
+    )
+  ));
+}
+
+
