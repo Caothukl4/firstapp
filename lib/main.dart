d04b377aeb981f3286bc39199ac0294d8e423bc5
Phạm Tuấn Anh <tuananhpvpl@gmail.com>
2025-03-05 23:19:23 +0700

    Stateless,statefull
    add stateless and hotreload

--- lib/main.dart
+++ lib/main.dart
@@ -3,30 +3,40 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Scaffold(
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text("My first app"),
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text("Hello TuanAnh",
-        style: TextStyle(
+        child: Text(
+          "Hello TuanAnh",
+          style: TextStyle(
             color: Colors.grey,
             fontSize: 20,
             fontWeight: FontWeight.bold,
             letterSpacing: 2.0,
-            fontFamily: 'IndieFlower'),
+            fontFamily: 'IndieFlower',
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
+        onPressed: () {},
         backgroundColor: Colors.red[600],
-        child: Text("Click",style: TextStyle(color: Colors.white),
+        child: Text(
+          "Click me",
+          style: TextStyle(color: Colors.white),
+        ),
       ),
-    )
-  ))
-  );
+    );
+  }
 }
-
-
